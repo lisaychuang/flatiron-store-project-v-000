50741811cfe2704d930901079ae215ccb2abe52e
Lisa Huang <lisa.yc.huang@gmail.com>
2018-01-23 08:54:18 -0800

    Updated StoreController @cart logic

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,5 +4,11 @@ class StoreController < ApplicationController
   def index
     @categories = Category.all
     @items = Item.available_items
+    if current_user
+      @cart = current_user.current_cart
+    else
+      @cart = nil
+    end
+
   end
 end
