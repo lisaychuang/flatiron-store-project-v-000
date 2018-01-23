28d6d6a397e72f76d0103076481a068126e0bb42
Lisa Huang <lisa.yc.huang@gmail.com>
2018-01-22 23:50:15 -0800

    Added status column to Cart table in migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123074235) do
+ActiveRecord::Schema.define(version: 20180123074859) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status",     default: "unsubmitted"
   end
 
   create_table "categories", force: :cascade do |t|
