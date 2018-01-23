4efa8b976d9aa87e5856fe509a5d71077a13d192
Lisa Huang <lisa.yc.huang@gmail.com>
2018-01-22 23:32:25 -0800

    Removed devise reset_token unique column. Used current_user and user_signed_in? helper

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,10 @@
 class StoreController < ApplicationController
+  # before_action :authenticate_user!
+
   def index
     @categories = Category.all
     @items = Item.available_items
+    # @user = current_user
     # @user = User.find(session[:user_id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,5 @@ ActiveRecord::Schema.define(version: 20180122232744) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", [nil], name: "index_users_on_reset_password_token", unique: true
 
 end
